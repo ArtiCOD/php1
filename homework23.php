1f0c946aa7ae6d239d771bfaafbf24886810a4ae
ArtiCOD <bulgakov97@mail.ru>
2023-03-18 12:26:59 +0300

    homework2

---
+++ homework23.php
@@ -0,0 +1,10 @@
+<?php
+//Задание 3
+$useranswer = (int)readline();
+while($useranswer <= 0) {
+    $useranswer = (int)readline();
+}
+for($i = 1; $i < $useranswer;$i++){
+
+}
+//тут мои полномочия всё
